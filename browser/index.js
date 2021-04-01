bf1e141068806d9f57e46860322cc62abcb074c6
yasin <buntyvirani82@gmail.com>
2021-03-31 22:04:53 -0700

    authentication is successfull

--- browser/index.js
+++ browser/index.js
@@ -7,21 +7,22 @@ async function startBrowser() {
     console.log("opening the browser.....");
     browser = await puppeteer.launch({
       headless: false,
-      excetuablePath:
-        "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+      // excetuablePath:
+      //   "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
     });
     const page = await browser.newPage();
-    await page.goto(
-      "https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin/"
-    );
+    // console.log("type of page", page);
+    await page.goto("https://www.linkedin.com");
+
     await page.waitFor(3000);
-    await page.click("#username");
-    // modify mail account
-    // await page.keyboard.type("YOUR_MAIL");
-    await page.click('[id="password"]');
-    // modify password account
-    // await page.keyboard.type("YOUR_PASSWORD");
+    await page.click("#session_key");
+
+    await page.keyboard.type("Enter_Your Email here");
+    await page.click('[id="session_password"]');
+
+    await page.keyboard.type("Enter_Your Password here");
     await page.click('[type="submit"]');
+    await page.waitFor(3000);
     await page.goto(
       "https://www.linkedin.com/search/results/people/?currentCompany=%5B%221586%22%2C%2217411%22%2C%2212227%22%2C%2249318%22%2C%22167364%22%2C%2216551%22%2C%2214951%22%2C%222649984%22%2C%2246825%22%2C%22208137%22%2C%2234924%22%2C%2261712%22%2C%22451028%22%2C%2247157%22%2C%22111446%22%2C%2221433%22%2C%222382910%22%2C%2271099%22%2C%22860467%22%5D&geoUrn=%5B%22102095887%22%5D&origin=FACETED_SEARCH",
       {
@@ -29,6 +30,7 @@ async function startBrowser() {
         waitUntil: "load",
       }
     );
+    // return;
   } catch (err) {
     console.log("Could not create a browser instance => :", err);
   }
--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const { startBrowser } = require("./browser");
 // const cherrio = require("cheerio");
 startBrowser();
 
-// async function scrapeData(url) {
-//   const browser = await startBrowser();
-//   const page = await browser.newPage();
-//   await page.goto(url, { waitUntil: "networkidle0" });
-//   await page.waitForSelector(".listResults");
-// }
+async function scrapeData(url) {
+  const browser = await startBrowser();
+  const page = await browser.newPage();
+  await page.goto(url, { waitUntil: "networkidle0" });
+  await page.waitForSelector(".listResults");
+}
 
-// // async function getBlogData(html) {
-// //   const blogData = {};
-// //   const $ = cherrio.load(html);
-// // }
-// scrapeData(
-//   "https://www.linkedin.com/search/results/people/?currentCompany=%5B%221586%22%2C%2217411%22%2C%2212227%22%2C%2249318%22%2C%22167364%22%2C%2216551%22%2C%2214951%22%2C%222649984%22%2C%2246825%22%2C%22208137%22%2C%2234924%22%2C%2261712%22%2C%22451028%22%2C%2247157%22%2C%22111446%22%2C%2221433%22%2C%222382910%22%2C%2271099%22%2C%22860467%22%5D&geoUrn=%5B%22102095887%22%5D&origin=FACETED_SEARCH"
-// );
+// async function getBlogData(html) {
+//   const blogData = {};
+//   const $ = cherrio.load(html);
+// }
+scrapeData(
+  "https://www.linkedin.com/search/results/people/?currentCompany=%5B%221586%22%2C%2217411%22%2C%2212227%22%2C%2249318%22%2C%22167364%22%2C%2216551%22%2C%2214951%22%2C%222649984%22%2C%2246825%22%2C%22208137%22%2C%2234924%22%2C%2261712%22%2C%22451028%22%2C%2247157%22%2C%22111446%22%2C%2221433%22%2C%222382910%22%2C%2271099%22%2C%22860467%22%5D&geoUrn=%5B%22102095887%22%5D&origin=FACETED_SEARCH"
+);
