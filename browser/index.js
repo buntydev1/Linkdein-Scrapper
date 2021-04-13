84c0a7a36e3be7df560e99e4332bd43b5a1c4433
yasin <buntyvirani82@gmail.com>
2021-04-13 01:11:49 -0700

    getting the designation of all employees from linkdein

--- browser/index.js
+++ browser/index.js
@@ -13,47 +13,37 @@ async function startBrowser() {
     const page = await browser.newPage();
     // console.log("type of page", page);
     await page.goto("https://www.linkedin.com");
-
     // await page.waitFor(3000);
     await page.click("#session_key");
 
-    await page.keyboard.type("Enter__Email");
+    await page.keyboard.type("");
     await page.click('[id="session_password"]');
 
-    await page.keyboard.type("Enter__Password");
+    await page.keyboard.type("");
     await page.click('[type="submit"]');
+
     await page.waitFor(3000);
-    // await page.goto(
-    //   "https://www.linkedin.com/search/results/people/?currentCompany=%5B%221586%22%2C%2217411%22%2C%2212227%22%2C%2249318%22%2C%22167364%22%2C%2216551%22%2C%2214951%22%2C%222649984%22%2C%2246825%22%2C%22208137%22%2C%2234924%22%2C%2261712%22%2C%22451028%22%2C%2247157%22%2C%22111446%22%2C%2221433%22%2C%222382910%22%2C%2271099%22%2C%22860467%22%5D&geoUrn=%5B%22102095887%22%5D&origin=FACETED_SEARCH",
-    //   {
-    //     timeout: 0,
-    //     waitUntil: "load",
-    //   }
+
+    await page.goto(
+      "https://www.linkedin.com/search/results/people/?currentCompany=%5B%221586%22%2C%2217411%22%2C%2212227%22%2C%2249318%22%2C%22167364%22%2C%2216551%22%2C%2214951%22%2C%222649984%22%2C%2246825%22%2C%22208137%22%2C%2234924%22%2C%2261712%22%2C%22451028%22%2C%2247157%22%2C%22111446%22%2C%2221433%22%2C%222382910%22%2C%2271099%22%2C%22860467%22%5D&geoUrn=%5B%22102095887%22%5D&origin=FACETED_SEARCH&page=17",
+      {
+        timeout: 0,
+        waitUntil: "load",
+      }
+    );
+
+    await page.waitForSelector(".entity-result__item");
+
+    // const getNames = await page.$$eval(
+    //   '.entity-result__title-text > a > span > span[aria-hidden="true"]',
+    //   (options) => options.map((option) => option.textContent)
     // );
-
-    const urls = [
-      "https://www.linkedin.com/search/results/people/?currentCompany=%5B%221586%22%2C%2217411%22%2C%2212227%22%2C%2249318%22%2C%22167364%22%2C%2216551%22%2C%2214951%22%2C%222649984%22%2C%2246825%22%2C%22208137%22%2C%2234924%22%2C%2261712%22%2C%22451028%22%2C%2247157%22%2C%22111446%22%2C%2221433%22%2C%222382910%22%2C%2271099%22%2C%22860467%22%5D&geoUrn=%5B%22102095887%22%5D&origin=FACETED_SEARCH",
-    ];
-
-    for (i = 2; i <= 100; i++) {
-      urls.push(
-        `https://www.linkedin.com/search/results/people/?currentCompany=%5B%221586%22%2C%2217411%22%2C%2212227%22%2C%2249318%22%2C%22167364%22%2C%2216551%22%2C%2214951%22%2C%222649984%22%2C%2246825%22%2C%22208137%22%2C%2234924%22%2C%2261712%22%2C%22451028%22%2C%2247157%22%2C%22111446%22%2C%2221433%22%2C%222382910%22%2C%2271099%22%2C%22860467%22%5D&geoUrn=%5B%22102095887%22%5D&origin=FACETED_SEARCH&page=${i}`
-      );
-      console.log("this is i", i);
-    }
-
-    for (i = 0; i < urls.length; i++) {
-      const url = urls[i];
-      await page.goto(url);
-
-      await page.waitForSelector(".entity-result__title-text");
-
-      const getNames = await page.$$eval(
-        '.entity-result__title-text > a > span > span[aria-hidden="true"]',
-        (options) => options.map((option) => option.textContent)
-      );
-      console.log("getThemAll", getNames);
-    }
+    // console.log("getThemAll", getNames);
+    const getDesignation = await page.$$eval(
+      ".entity-result__item > div.entity-result__content > div > div > div:nth-child(2) > div.entity-result__primary-subtitle",
+      (option) => option.map((options) => options.innerText)
+    );
+    console.log("getDesignation", getDesignation);
   } catch (err) {
     console.log("Could not create a browser instance => :", err);
   }
--- index.js
+++ index.js
@@ -2,35 +2,3 @@ const { startBrowser } = require("./browser");
 const cherrio = require("cheerio");
 startBrowser();
 
-// async function scrapeData(url) {
-//   console.log("this is scrapeData", scrapeData);
-//   const browser = await startBrowser();
-//   // const page = await browser.newPage();
-//   await page.goto(url, { waitUntil: "networkidle0" });
-//   console.log("before ");
-//   await page.waitForSelector(".search-results-container");
-//   console.log("after");
-//   // console.log("this is", typeof waitForSelector);
-//   const content = await page.content();
-//   await getNameData(content);
-//   console.log("this is content", content);
-//   await browser.close();
-// }
-
-// async function getNameData(html) {
-//   const nameData = {};
-//   const $ = cherrio.load(html);
-//   $(".entity-result").each((i, element) => {
-//     console.log(
-//       $(element)
-//         .waitForSelector(
-//           "div.entity-result__content entity-result__divider span.entity-result__title-text"
-//         )
-//         .find("span")
-//         .text()
-//     );
-//   });
-// }
-// scrapeData(
-//   "https://www.linkedin.com/search/results/people/?currentCompany=%5B%221586%22%2C%2217411%22%2C%2212227%22%2C%2249318%22%2C%22167364%22%2C%2216551%22%2C%2214951%22%2C%222649984%22%2C%2246825%22%2C%22208137%22%2C%2234924%22%2C%2261712%22%2C%22451028%22%2C%2247157%22%2C%22111446%22%2C%2221433%22%2C%222382910%22%2C%2271099%22%2C%22860467%22%5D&geoUrn=%5B%22102095887%22%5D&origin=FACETED_SEARCH"
-// );
