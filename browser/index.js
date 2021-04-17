365165a2e32894aa8dd59f0cc4e4ee366fb565df
yasin <buntyvirani82@gmail.com>
2021-04-17 07:19:57 -0700

    getting data in csv format from one page

--- browser/index.js
+++ browser/index.js
@@ -1,5 +1,7 @@
 const puppeteer = require("puppeteer");
-var fs = require("fs");
+
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+
 async function startBrowser() {
   let browser;
 
@@ -31,13 +33,26 @@ async function startBrowser() {
         waitUntil: "load",
       }
     );
+    // const urls = [
+
+    //   "https://www.linkedin.com/search/results/people/?currentCompany=%5B%221586%22%2C%2217411%22%2C%2212227%22%2C%2249318%22%2C%22167364%22%2C%2216551%22%2C%2214951%22%2C%222649984%22%2C%2246825%22%2C%22208137%22%2C%2234924%22%2C%2261712%22%2C%22451028%22%2C%2247157%22%2C%22111446%22%2C%2221433%22%2C%222382910%22%2C%2271099%22%2C%22860467%22%5D&geoUrn=%5B%22102095887%22%5D&origin=FACETED_SEARCH",
+    // ];
+
+    // for (i = 2; i <= 100; i++) {
+    //   urls.push(
+    //     `https://www.linkedin.com/search/results/people/?currentCompany=%5B%221586%22%2C%2217411%22%2C%2212227%22%2C%2249318%22%2C%22167364%22%2C%2216551%22%2C%2214951%22%2C%222649984%22%2C%2246825%22%2C%22208137%22%2C%2234924%22%2C%2261712%22%2C%22451028%22%2C%2247157%22%2C%22111446%22%2C%2221433%22%2C%222382910%22%2C%2271099%22%2C%22860467%22%5D&geoUrn=%5B%22102095887%22%5D&origin=FACETED_SEARCH&page=${i}`
+    //   );
+    //   console.log("this is i", i);
+    // }
+    // await page.waitFor(3000);
+
+    // for (i = 0; i < urls.length; i++) {
+    //   const url = urls[i];
+    //   await page.goto(url);
+
     await page.waitForSelector("ul.reusable-search__entity-results-list");
 
-    // const names = await page.$$eval(
-    //   "li.reusable-search__result-container",
-    //   (options) => options.map((option) => option.innerText)
-    // );
-    const names = await page.$$eval(
+    const allData = await page.$$eval(
       "li.reusable-search__result-container",
       (options) =>
         options.map((option) => {
@@ -57,15 +72,26 @@ async function startBrowser() {
         })
     );
 
-    console.log("this is option", names);
+    console.log("this is option", allData);
 
-    const JSONOBJECT = JSON.stringify(names);
-    console.log("this is object", JSONOBJECT);
-
-    fs.writeFile("myjsonfile.json", JSONOBJECT, function (err) {
-      if (err) throw err;
-      console.log("complete");
+    const csvWriter = createCsvWriter({
+      path: "out.csv",
+      header: [
+        { id: "name", title: "Name" },
+        { id: "designation", title: "Designation" },
+        { id: "location", title: "Location" },
+        { id: "profileURL", title: "profileURL" },
+      ],
     });
+    csvWriter
+      .writeRecords(allData)
+      .then(() => console.log("The CSV file was written successfully"));
+    jsonexport(allData, function (err, csv) {
+      if (err) return console.error(err);
+      console.log(csv);
+    });
+
+    // }
   } catch (err) {
     console.log("Could not create a browser instance => :", err);
   }
