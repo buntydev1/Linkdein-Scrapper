ff80fa53f957188abad10601dbd9caea1b07ef94
yasin <buntyvirani82@gmail.com>
2021-04-18 03:08:29 -0700

    getting all the employess information from linkdein

--- browser/index.js
+++ browser/index.js
@@ -18,7 +18,7 @@ async function startBrowser() {
   try {
     console.log("opening the browser.....");
     browser = await puppeteer.launch({
-      headless: false,
+      headless: true,
       // excetuablePath:
       //   "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
     });
@@ -31,7 +31,7 @@ async function startBrowser() {
     await page.keyboard.type("");
     await page.click('[id="session_password"]');
 
-    await page.keyboard.type("3");
+    await page.keyboard.type("");
     await page.click('[type="submit"]');
 
     await page.waitFor(3000);
@@ -47,27 +47,42 @@ async function startBrowser() {
       "https://www.linkedin.com/search/results/people/?currentCompany=%5B%221586%22%2C%2217411%22%2C%2212227%22%2C%2249318%22%2C%22167364%22%2C%2216551%22%2C%2214951%22%2C%222649984%22%2C%2246825%22%2C%22208137%22%2C%2234924%22%2C%2261712%22%2C%22451028%22%2C%2247157%22%2C%22111446%22%2C%2221433%22%2C%222382910%22%2C%2271099%22%2C%22860467%22%5D&geoUrn=%5B%22102095887%22%5D&origin=FACETED_SEARCH",
     ];
 
-    for (i = 2; i <= 3; i++) {
+    for (i = 58; i <= 59; i++) {
       urls.push(
         `https://www.linkedin.com/search/results/people/?currentCompany=%5B%221586%22%2C%2217411%22%2C%2212227%22%2C%2249318%22%2C%22167364%22%2C%2216551%22%2C%2214951%22%2C%222649984%22%2C%2246825%22%2C%22208137%22%2C%2234924%22%2C%2261712%22%2C%22451028%22%2C%2247157%22%2C%22111446%22%2C%2221433%22%2C%222382910%22%2C%2271099%22%2C%22860467%22%5D&geoUrn=%5B%22102095887%22%5D&origin=FACETED_SEARCH&page=${i}`
       );
-      console.log("this is i", i);
+      // console.log("this is i", i);
     }
 
     for (i = 0; i < urls.length; i++) {
       const url = urls[i];
+      console.log("this is i", i);
       await page.goto(url);
 
       await page.waitForSelector("ul.reusable-search__entity-results-list");
-
+      await page.screenshot({
+        path: `./ss/afterLoaded${i}.jpeg`,
+        fullPage: true,
+      });
       const allData = await page.$$eval(
         "li.reusable-search__result-container",
-        (options) =>
-          options.map((option) => {
+        (options) => {
+          const getName = (option) => {
+            const linkedInMember = option
+              .querySelector(".entity-result__title-text > a")
+              .innerText.toLowerCase();
+
+            return linkedInMember === "linkedin member"
+              ? linkedInMember
+              : option
+                  .querySelector(
+                    ".entity-result__title-text > a > span > span:nth-child(1)"
+                  )
+                  .innerText.trim();
+          };
+          return options.map((option) => {
             return (obj = {
-              name: option
-                .querySelector(".entity-result__title-text > a ")
-                .innerText.trim(),
+              name: getName(option),
               profileURL: option.querySelector(
                 "div.entity-result__content > div > div > div> span > div > span > span > a"
               ).href,
@@ -78,10 +93,11 @@ async function startBrowser() {
                 ".entity-result__item > div.entity-result__content > div > div >  div:nth-child(2) > div.entity-result__secondary-subtitle"
               ).innerText,
             });
-          })
+          });
+        }
       );
 
-      console.log("this is option", allData);
+      // console.log("this is option", allData);
 
       // const csvWriter = createCsvWriter({
       //   path: "out.csv",
@@ -97,7 +113,7 @@ async function startBrowser() {
       //   .then(() => console.log("The CSV file was written successfully"));
 
       const csv = converToCSV(allData);
-      console.log("🚀 ~ file: index.js ~ line 100 ~ startBrowser ~ csv", csv);
+      // console.log("🚀 ~ file: index.js ~ line 100 ~ startBrowser ~ csv", csv);
 
       fs.appendFile("out.csv", csv, (err) => {
         if (err) console.error("Couldn't append the data");
@@ -105,6 +121,7 @@ async function startBrowser() {
       });
     }
   } catch (err) {
+    // await page.screenshot({ path: `screenshots/globalError.jpeg` });
     console.log("Could not create a browser instance => :", err);
   }
   return browser;
