a52a602325c1777594cbe41de1ee96e740037f8e
yasin <buntyvirani82@gmail.com>
2021-04-19 04:08:00 -0700

    duplicated names removed from employees.json

--- filteredNames.js
+++ filteredNames.js
@@ -1,3 +1,25 @@
-const { startBrowser } = require("./browser");
-startBrowser();
-const puppeteer = require("puppeteer");
+// const { startBrowser } = require("./browser");
+// startBrowser();
+var fs = require("fs");
+
+const data = require("./employees.json");
+
+// var uniqueValue = new Set(data);
+
+// var newdata = {};
+// newdata.table = [];
+// for (i = 0; i < uniqueValue.length; i++) {
+//   newdata.table.push(uniqueValue);
+// }
+jsonObject = data.map(JSON.stringify);
+
+uniqueSet = new Set(jsonObject);
+uniqueArray = Array.from(uniqueSet);
+
+fs.writeFile("input1.json", uniqueArray, function (error) {
+  if (error) {
+    console.error("write error:  " + error.message);
+  } else {
+    console.log("Successful Write to ");
+  }
+});
